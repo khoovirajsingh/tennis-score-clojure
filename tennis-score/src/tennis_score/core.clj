e9ace616ea1175ebcbd732e60e611653a26cb1b3
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2020-06-18 20:44:24 +0100

    running score for different points

--- tennis-score/src/tennis_score/core.clj
+++ tennis-score/src/tennis_score/core.clj
@@ -1,11 +1,13 @@
 (ns tennis-score.core
   (:gen-class))
 
-(defn point-to-score-description
+(defn point-to-score
   [point]
-  ({0 "Love", 1 "Fifteen", 2 "Thirty", 3 "Fourty"} point))
+  ({0 "Love", 1 "Fifteen", 2 "Thirty", 3 "Forty"} point))
 
-
+(defn running-score
+  [playerOne playerTwo]
+  (str (point-to-score playerOne) "-" (point-to-score playerTwo)))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- tennis-score/test/tennis_score/core_test.clj
+++ tennis-score/test/tennis_score/core_test.clj
@@ -2,5 +2,9 @@
   (:require [clojure.test :refer :all]
             [tennis-score.core :refer :all]))
 
-(deftest point-is-mapped-to-score-description
-  (is (= "Love" (point-to-score-description 0))))
+(deftest individual-point-is-mapped-to-score-description
+  (is (= "Love" (point-to-score 0))))
+
+(deftest display-score-when-points-are-different-for-both-players
+  (is (= "Love-Fifteen" (running-score 0 1)))
+  (is (= "Forty-Thirty" (running-score 3 2))))
